d250bbabf38fe7c7e4b663390894e0a8a2a8bcbb
RasmusHaug <97478497+RasmusHaug@users.noreply.github.com>
2025-01-23 14:42:23 +0200

    Initial Commit. Project has separet backend and frontend folders with respective projects setup Front-end: Vite + React + Tailwind CSS Back-end: Java Spring Boot using Gradel.

---
+++ backend/src/main/java/enefit/rasmushaug/assignment/enefitPowerApp/EnefitPowerAppApplication.java
@@ -0,0 +1,13 @@
+package enefit.rasmushaug.assignment.enefitPowerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnefitPowerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnefitPowerAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/enefit/rasmushaug/assignment/enefitPowerApp/EnefitPowerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package enefit.rasmushaug.assignment.enefitPowerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnefitPowerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
