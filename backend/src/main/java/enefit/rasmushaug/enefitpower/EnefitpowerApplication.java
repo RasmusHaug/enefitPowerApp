d405398314cf83df4ef9871721c11eb2d9cf9af0
RasmusHaug <97478497+RasmusHaug@users.noreply.github.com>
2025-01-23 16:39:07 +0200

    Update package name to match regex convention

---
+++ backend/src/main/java/enefit/rasmushaug/enefitpower/EnefitpowerApplication.java
@@ -0,0 +1,13 @@
+package enefit.rasmushaug.enefitpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnefitpowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnefitpowerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/enefit/rasmushaug/enefitpower/EnefitpowerApplicationTests.java
@@ -0,0 +1,13 @@
+package enefit.rasmushaug.enefitpower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnefitpowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
