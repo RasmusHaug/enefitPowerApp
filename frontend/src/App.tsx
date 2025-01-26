3a2d03fcb7f1b2356969ab9a38fbdc9df2cf26f3
RasmusHaug <97478497+RasmusHaug@users.noreply.github.com>
2025-01-26 19:39:04 +0200

    Added new bar chart element to front-end Recharts This is used to display bar charts of electricity price and customer electricity usage.,

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -225,7 +225,7 @@ const App: React.FC = () => {
             </div>
         </header>
         <main>
-            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <div className="text-white mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 {renderContent()}
             </div>
         </main>
--- frontend/src/pages/Overview.tsx
+++ frontend/src/pages/Overview.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+        name: 'Esmaspäev',
+        elektri_hind: 4000,
+    },
+    {
+        name: 'Teisipäev',
+        elektri_hind: 4000,
+    },
+    {
+        name: 'Kolmapäev',
+        elektri_hind: 4000,
+    },
+    {
+        name: 'Neljapäev',
+        elektri_hind: 4000,
+    },
+    {
+        name: 'Reede',
+        elektri_hind: 4000,
+    },
+    {
+        name: 'Laupäev',
+        elektri_hind: 4000,
+    },
+    {
+        name: 'Pühapäev',
+        elektri_hind: 4000,
+    },
+];
 
 const Overview: React.FC = () => {
     return (
-        <div>
-            <h2 className="text-xl font-semibold">Ülevaade Content</h2>
-            <p>This is the content for the Ülevaade page.</p>
-        </div>
+        <div className="p-5 bg-slate-900 rounded-md">
+            <h1 className="text-2xl font-bold text-white mb-4">Elektri Hind viimane nädal</h1>
+            <ResponsiveContainer width="100%" height={300}>
+                <BarChart data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                {/* Bar for elektri_hind */}
+                <Bar dataKey="elektri_hind" fill="#4c51bf" />
+                {/* Bar for elektri_kulu */}
+                <Bar dataKey="elektri_kulu" fill="#63b3ed" />
+                </BarChart>
+            </ResponsiveContainer>
+    </div>
     );
 };
 
