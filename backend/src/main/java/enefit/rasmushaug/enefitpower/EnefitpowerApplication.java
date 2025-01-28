0c6f1c70baaae4ee1105b87e9084f284b6cf8a40
RasmusHaug <97478497+RasmusHaug@users.noreply.github.com>
2025-01-28 15:07:55 +0200

    Update aded dotenv to project Using dotenv to load variables from .env file

--- backend/src/main/java/enefit/rasmushaug/enefitpower/EnefitpowerApplication.java
+++ backend/src/main/java/enefit/rasmushaug/enefitpower/EnefitpowerApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class EnefitpowerApplication {
 
 	public static void main(String[] args) {
+		Dotenv.load();
 		SpringApplication.run(EnefitpowerApplication.class, args);
 	}
 
--- backend/src/main/java/enefit/rasmushaug/enefitpower/components/JwtUtil.java
+++ backend/src/main/java/enefit/rasmushaug/enefitpower/components/JwtUtil.java
@@ -2,17 +2,19 @@ package enefit.rasmushaug.enefitpower.components;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 
+
 @Component
 public class JwtUtil {
-    // TODO: Random Testing Secret Key.
-    // TODO: IN PRODUCTION take this from environment variable!!!
-    private static final String secretKey = "9e4tvChthvAnBwJrH4duembUtWZzTr";
+
+    @Value("${JWT_UTIL_SECRET_KEY}")
+    private String secretKey;
 
     public String generatedToken(String username) {
         return JWT.create()
